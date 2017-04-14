432cba6abbc0b70c96b00e378bd2bd5e12303946
Saurav Yadav <sauravyadav1286@gmail.com>
2017-04-14 06:09:53 +0530

    CoreJava_Session3Assignment2
    CoreJava_Session3Assignment2 Description

---
+++ Example.java
@@ -0,0 +1,17 @@
+class Example 
+{
+ int x; int y;
+ String name;
+	 public static void main(String args[])
+	  {
+ 		Example pnt = new Example();
+ 		System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	  } 
+}
+
+
+/* This program Prints "pnt is null 0 0." as the instance variables are 
+assigned with the default values . String will be assigned null and Integer will 
+be assigned 0 */
+
+// So the correct option is C 
